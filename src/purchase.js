936b95534059cdccc25b6307f609769e070af6ed
JP Segurado <jpmsegurado@gmail.com>
2020-01-26 10:11:07 -0300

    refactor(purchase): added functions to scope for better testing

--- src/purchase.js
+++ src/purchase.js
@@ -8,45 +8,45 @@ export default function Purchase () {
   }
 
   this.create = ({ input }) => {
-    if (!isValid(input)) throw new Error('Invalid purchase input')
+    if (!this.isValid(input)) throw new Error('Invalid purchase input')
 
     this.input = input
-    this.type = getType(input)
-    const { amount, good, price } = breakInputIntoGoodAmountPrice(this.input)
+    this.type = this.getType(input)
+    const { amount, good, price } = this.breakInputIntoGoodAmountPrice(this.input)
     this.amount = amount
     this.good = new Good().create({ name: good })
     this.price = price
     return this
   }
 
-  function isRegular (input) {
+  this.isRegular = (input) => {
     return /[0-9]{1,5}\s[\a-zA-Z\s]*\sat\s[\[0-9]+(\.[0-9]+)?$]*/.test(input)
   }
 
-  function isRegularImported (input) {
+  this.isRegularImported = (input) => {
     return /[0-9]{1,5}\simported\s[\a-zA-Z\s]*\sat\s[\[0-9]+(\.[0-9]+)?$]*/.test(input)
   }
 
-  function isImportedOfSomething (input) {
+  this.isImportedOfSomething = (input) => {
     return /[0-9]{1,5}\s[\a-zA-Z\s]*\sof\simported\s[\a-zA-Z\s]*\sat\s[\[0-9]+(\.[0-9]+)?$]*/.test(input)
   }
 
-  function isImported (input) {
-    return isRegularImported(input) || isImportedOfSomething(input)
+  this.isImported = (input) => {
+    return this.isRegularImported(input) || this.isImportedOfSomething(input)
   }
 
-  function isValid (input) {
-    return isImported(input) || isRegular(input)
+  this.isValid = (input) => {
+    return this.isImported(input) || this.isRegular(input)
   }
 
-  function getType (input) {
-    if (isRegularImported(input)) return types.regularImported
-    if (isImportedOfSomething(input)) return types.importedOfSomething
+  this.getType = (input) => {
+    if (this.isRegularImported(input)) return types.regularImported
+    if (this.isImportedOfSomething(input)) return types.importedOfSomething
 
     return types.regular
   }
 
-  function breakInputIntoGoodAmountPrice (input) {
+  this.breakInputIntoGoodAmountPrice = (input) => {
     const amountRegex = /[0-9]{1,5}\s/
     const amount = parseInt(input.match(amountRegex)[0])
     const goodRegex = /\s[\a-zA-Z\s]*at/
@@ -56,59 +56,59 @@ export default function Purchase () {
     return { amount, good, price }
   }
 
-  function calculateTaxByGoodType (type) {
+  this.calculateTaxByGoodType = (type) => {
     return type === goodTypes.miscelaneous ? 0.1 : 0
   }
 
-  function calculateImportedTax (type) {
+  this.calculateImportedTax = (type) => {
     return type === types.importedOfSomething || type === types.regularImported
     ? 0.05
     : 0
   }
 
-  function roundToNearest (number) {
+  this.roundToNearest = (number) => {
     return Math.ceil((number) * 20)/20
   }
 
-  function roundToTwoDecimals (number) {
+  this.roundToTwoDecimals = (number) => {
     return Math.round(number * 100) / 100
   }
 
-  function shouldRoundToNearest (number) {
+  this.shouldRoundToNearest = (number) => {
     const decimals = number.toFixed(2).split('.')[1]
     const lastDecimal = parseInt(decimals.charAt(1))
     return lastDecimal < 5
   }
 
-  function calculateTaxes ({ good, type, price }) {
-    const goodTax = calculateTaxByGoodType(good.type)
-    const importedTax = calculateImportedTax(type)
+  this.calculateTaxes = ({ good, type, price }) => {
+    const goodTax = this.calculateTaxByGoodType(good.type)
+    const importedTax = this.calculateImportedTax(type)
     const taxes = goodTax + importedTax
     const taxValue = price * taxes
     return taxValue
   }
 
-  function calculateTotal ({ good, amount, type, price }) {
-    const initialTaxValue = calculateTaxes({ good, type, price })
+  this.calculateTotal = ({ good, amount, type, price }) => {
+    const initialTaxValue = this.calculateTaxes({ good, type, price })
     const priceWithTaxes = price + initialTaxValue
-    const rounded = shouldRoundToNearest(priceWithTaxes)
-      ? roundToNearest(priceWithTaxes)
-      : roundToTwoDecimals(priceWithTaxes)
+    const rounded = this.shouldRoundToNearest(priceWithTaxes)
+      ? this.roundToNearest(priceWithTaxes)
+      : this.roundToTwoDecimals(priceWithTaxes)
     return rounded * amount
   }
 
   this.getTotal = () => {
-    return calculateTotal(this)
+    return this.calculateTotal(this)
   }
 
   this.getReceipt = () => {
-    const result = `${this.amount} ${this.good.name}: ${calculateTotal(this).toFixed(2)}`
+    const result = `${this.amount} ${this.good.name}: ${this.calculateTotal(this).toFixed(2)}`
     return result
   }
 
   this.getTaxAmount = () => {
     const totalWithoutTaxes = this.price * this.amount
-    const total = calculateTotal(this)
-    return roundToTwoDecimals(total - totalWithoutTaxes)
+    const total = this.calculateTotal(this)
+    return this.roundToTwoDecimals(total - totalWithoutTaxes)
   }
 }
